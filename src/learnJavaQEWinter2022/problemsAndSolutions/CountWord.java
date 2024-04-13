26cc842cc2dba1a39cf5dbb3faed5bb0e5d81147
Shebleymsi <Shebley2022@gmail.com>
2024-04-13 19:32:53 -0400

    1st commit

---
+++ src/learnJavaQEWinter2022/problemsAndSolutions/CountWord.java
@@ -0,0 +1,10 @@
+package learnJavaQEWinter2022.problemsAndSolutions;
+
+public class CountWord {
+
+    public static void main(String[] args) {
+        System.out.println("Alhamdolillah");
+    }
+
+
+}
